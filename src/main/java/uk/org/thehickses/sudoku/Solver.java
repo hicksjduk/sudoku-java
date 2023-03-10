6a7044d30ca1c5f51ab177fa3d96dee6df292aa9
Jeremy Hicks <jeremy@thehickses.org.uk>
2023-03-10 15:08:30 +0000

    Refactor

--- src/main/java/uk/org/thehickses/sudoku/Solver.java
+++ src/main/java/uk/org/thehickses/sudoku/Solver.java
@@ -6,9 +6,9 @@ import java.util.stream.Stream;
 
 public class Solver
 {
-    static final Solver defaultSolver = new Solver(IntStream.rangeClosed(1, 9), 0);
+    public static final Solver defaultSolver = new Solver();
 
-    static final Grid puzzle = defaultSolver.gridWith(8, 0, 0, 0, 0, 0, 0, 0, 0)
+    public static final Grid puzzle = defaultSolver.gridWith(8, 0, 0, 0, 0, 0, 0, 0, 0)
             .and(0, 0, 3, 6, 0, 0, 0, 0, 0)
             .and(0, 7, 0, 0, 9, 0, 2, 0, 0)
             .and(0, 5, 0, 0, 0, 7, 0, 0, 0)
